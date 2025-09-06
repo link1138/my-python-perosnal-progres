cd5b6c60eac09442f146066e6763e94c581ccef4
link1138 <75946304+link1138@users.noreply.github.com>
2025-09-06 14:33:27 -0400

    First little test script i made
    Also decided to use it as a "prank" of sort

---
+++ insultprompt.py
@@ -0,0 +1,10 @@
+#insert your own name in place of the badname deffinition
+badname = "name"
+#space
+name_select = input("What Is Your Name ")
+#space
+if name_select.lower() == badname:
+    print("You are a stupid")
+#space
+else:
+    print("You are Smart")
